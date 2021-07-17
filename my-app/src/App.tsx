40677694bf9ab1cbadf84aab11ead6a51ef92f30
Alex <74793187+MasterTatto@users.noreply.github.com>
2021-07-17 15:36:21 +0300

    ready half

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,8 @@
 import React, {useState} from 'react';
 
 import './App.css';
-import {v1} from "uuid";
 import TodoList from "./TodoList";
-import InputForm from "./InputForm";
-import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
+import {v1} from "uuid";
 
 export type FilterValue = 'all' | 'completed' | 'active'
 export type TaskType = {
@@ -19,132 +16,83 @@ type TaskTypeProps = {
 export type TodoListType = {
     id: string
     title: string
-    filter: string
+    filter: FilterValue
 }
 
 function App() {
+    const todoListId_1 = v1()
+    const todoListId_2 = v1()
 
-    const todoID_1 = v1()
-    const todoID_2 = v1()
-    //
-    const [tasks, setTasks] = useState<TaskTypeProps>({
-        [todoID_1]: [
-            {id: v1(), title: 'CSS', isDone: true},
-            {id: v1(), title: 'HTML', isDone: true},
-            {id: v1(), title: 'REACT', isDone: false},
-            {id: v1(), title: 'REDUX', isDone: false},
-        ], [todoID_2]: [
-            {id: v1(), title: 'bread', isDone: true},
-            {id: v1(), title: 'Milk', isDone: true},
-            {id: v1(), title: 'cheeps', isDone: false},
-        ]
-    })
-    //
-    const [todoLists, setTodoLists] = useState<Array<TodoListType>>([
-        {id: todoID_1, title: 'What to learn', filter: 'all'},
-        {id: todoID_2, title: 'What to buy', filter: 'all'},
+    let [todoLists, setTodoList] = useState<Array<TodoListType>>([
+        {id: todoListId_1, title: 'What to learn', filter: 'all'},
+        {id: todoListId_2, title: 'What to buy', filter: 'all'},
     ])
 
+    const [tasks, setTasks] = useState<TaskTypeProps>({
+        [todoListId_1]: [
+            {id: v1(), title: 'CSS', isDone: false},
+            {id: v1(), title: 'JS', isDone: true},
+            {id: v1(), title: 'TS', isDone: false},
+            {id: v1(), title: 'REACT', isDone: true}],
 
-    function removeTask(todoID: string, id: string) {
-        tasks[todoID] = tasks[todoID].filter((f) => {
-            return f.id !== id;
-        })
-        setTasks({...tasks})
-    }
+        [todoListId_2]: [
+            {id: v1(), title: 'bread', isDone: false},
+            {id: v1(), title: 'cheeps', isDone: true},
+            {id: v1(), title: 'potate', isDone: false},
+        ],
+    })
 
-    function editSpanTitle(todoID: string, id: string, title: string) {
-        tasks[todoID] = tasks[todoID].map((t) => {
-            if (t.id === id) {
-                return {...t, title}
-            } else {
-                return t
-            }
-        })
-        setTasks({...tasks})
-    }
-
-    function addTask(todoID: string, title: string) {
-        const newTask = {id: v1(), title, isDone: false}
-        tasks[todoID] = [newTask, ...tasks[todoID]]
-        setTasks({...tasks})
-    }
-
+    //
     function changeChecked(todoID: string, id: string, bool: boolean) {
-        tasks[todoID] = tasks[todoID].map(t => t.id === id ? {...t, isDone: bool} : t)
+        tasks[todoID] = tasks[todoID].map((f) => f.id === id ? {...f, isDone: bool} : f)
         setTasks({...tasks})
     }
 
     //
+    function removeTasks(todoID: string, id: string) {
+        tasks[todoID] = tasks[todoID].filter((f) => f.id !== id)
+        setTasks({...tasks})
+    }
+
     //
+    function changeFilterBtn(filter: FilterValue, todoID: string) {
+        let newFilter = todoLists.map(f => f.id === todoID ? {...f, filter: filter} : f)
+        setTodoList(newFilter)
+    }
+
     //
-    function addTodoList(title: string) {
-        const todoID = v1()
-        const newTodoList = {id: todoID, title, filter: 'all'}
-        setTodoLists([newTodoList, ...todoLists])
-        setTasks({...tasks, [todoID]: []})
+    function addTask(todoID: string, title: string) {
+        const newTasks = {id: v1(), title, isDone: false}
+        tasks[todoID] = [newTasks, ...tasks[todoID]]
+        setTasks({...tasks})
     }
 
-    function removeTodo(todoID: string) {
-        const removeList = todoLists.filter(f => f.id !== todoID)
-        setTodoLists(removeList)
-        delete tasks[todoID]
-    }
-
-    function changeTodoListFilter(f: FilterValue, todoID: string) {
-        const newFilter = todoLists.map(t => t.id === todoID ? {...t, filter: f} : t)
-        setTodoLists(newFilter)
-    }
-
-    function editSpanTodo(todoID: string, title: string) {
-        const newTitle = todoLists.map((tl) => {
-            if (tl.id === todoID) {
-                return {...tl, title}
-            } else {
-                return tl
-            }
-        })
-        setTodoLists(newTitle)
-    }
-
+    //
     return (
         <div>
-            <AppBar position="static">
-                <Toolbar style={{display: 'flex', justifyContent: 'space-between'}}>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        <span style={{fontSize: '30px', fontWeight: 'bold'}}>TodoList</span>
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-            <div className="App">
-
-                <InputForm addItem={addTodoList}/>
-                {todoLists.map((tl) => {
+            {
+                todoLists.map((tl) => {
                     function windowTasks() {
                         if (tl.filter === 'active') {
-                            return tasks[tl.id].filter(f => !f.isDone)
+                            return tasks[tl.id].filter(f => f.isDone);
                         }
                         if (tl.filter === 'completed') {
-                            return tasks[tl.id].filter(f => f.isDone)
+                            return tasks[tl.id].filter(f => !f.isDone)
                         } else {
                             return tasks[tl.id]
                         }
                     }
 
-                    return <TodoList title={tl.title} tasks={windowTasks()} key={tl.id} removeTask={removeTask}
-                                     id={tl.id}
-                                     removeTodo={removeTodo}
-                                     editSpanTodo={editSpanTodo}
-                                     editSpanTitle={editSpanTitle}
-                                     filterButton={changeTodoListFilter} addTask={addTask}
-                                     changeChecked={changeChecked}/>
-                })}
-
-            </div>
+                    return <TodoList
+                        tasks={windowTasks()}
+                        key={tl.id} title={tl.title}
+                        id={tl.id} removeTasks={removeTasks}
+                        changeChecked={changeChecked}
+                        changeFilterBtn={changeFilterBtn}
+                        addTask={addTask}
+                    />
+                })
+            }
         </div>
     );
 }
--- my-app/src/TodoList.tsx
+++ my-app/src/TodoList.tsx
@@ -1,80 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {FilterValue, TaskType} from "./App";
-import InputForm from "./InputForm";
-import EditSpan from "./EditSpan";
-import {Button, Checkbox, Grid} from "@material-ui/core";
-import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
-import DeleteForeverOutlinedIcon from '@material-ui/icons/DeleteForeverOutlined';
 
-type TodoListPropsType = {
-    title: string
+type todoPropsType = {
     tasks: Array<TaskType>
-    removeTask: (todoID: string, id: string) => void
+    title: string
     id: string
-    filterButton: (f: FilterValue, todoID: string) => void
-    addTask: (todoID: string, title: string) => void
+    removeTasks: (todoID: string, id: string) => void
     changeChecked: (todoID: string, id: string, bool: boolean) => void
-    removeTodo: (todoId: string) => void
-    editSpanTitle: (todoID: string, id: string, title: string) => void
-    editSpanTodo: (todoID: string, title: string) => void
-
+    changeFilterBtn:(filter:FilterValue,id:string) => void
+    addTask:(todoID:string,title:string) => void
 }
-//
-const TodoList = (props: TodoListPropsType) => {
+const TodoList = (props: todoPropsType) => {
+
     const liItem = props.tasks.map((t) => {
-        //
-        function editSpanTask(title: string) {
-            props.editSpanTitle(props.id, t.id, title)
-        }
-
-        //
         return <li key={t.id}>
-            <Checkbox checked={t.isDone}
-                      onChange={(e) => props.changeChecked(props.id, t.id, e.currentTarget.checked)}/>
-            <EditSpan title={t.title} editSpanTask={editSpanTask}/>
-            <Button variant="outlined" color="primary" size={'small'}
-                    onClick={() => props.removeTask(props.id, t.id)}><DeleteForeverOutlinedIcon/></Button>
-            <Grid item xs={8}>
-
-
-            </Grid>
+            <input type="checkbox" checked={t.isDone}
+                   onChange={(e) => props.changeChecked(props.id, t.id, e.currentTarget.checked)}/>
+            {t.title}
+            <button onClick={() => props.removeTasks(props.id, t.id)}>X</button>
         </li>
+
     })
-
-    //
-    function addTask(title: string) {
-        props.addTask(props.id, title)
-
-    }
-
-    //
-    function changeTitleTodo(title: string) {
-        props.editSpanTodo(props.id, title)
-    }
-
+//
+    const [value,setValue] = useState('')
     //
     return (
-        <div className={'todo'}>
+        <div className={'tl'}>
             <div>
-                <h1><EditSpan title={props.title} editSpanTask={changeTitleTodo}/>
-                    <Button variant="contained"
-                            color="secondary"
-                        // className={classes.button}
-                            startIcon={<DeleteOutlinedIcon/>}
-                            onClick={() => props.removeTodo(props.id)}/>
-                </h1>
+                <span>{props.title}
+                    <button>X</button></span>
             </div>
-            <InputForm addItem={addTask}/>
+            <div>
+                <input type="text" value={value} onChange={(e) => setValue(e.currentTarget.value)}/>
+                <button onClick={() => {
+                    props.addTask(props.id, value)
+                    setValue('')
+                }}>add</button>
+            </div>
             <div>
                 <ul>
                     {liItem}
                 </ul>
             </div>
-            <Button variant="contained" onClick={() => props.filterButton('all', props.id)}>All</Button>
-            <Button variant="contained" color="primary"
-                    onClick={() => props.filterButton('active', props.id)}>Active</Button>
-            <Button variant="contained" color="secondary"
-                    onClick={() => props.filterButton('completed', props.id)}>Completed</Button>
+            <div>
+                <button onClick={() => props.changeFilterBtn('all' , props.id)}>All</button>
+                <button onClick={() => props.changeFilterBtn('active' , props.id)}>Completed</button>
+                <button onClick={() => props.changeFilterBtn('completed' , props.id)}>Active</button>
+            </div>
         </div>
     );
 };
