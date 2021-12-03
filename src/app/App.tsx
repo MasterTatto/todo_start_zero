4ea83d21c752c8516c711208ea8774341135b3ae
alex <gromvolk97@gmail.com>
2021-12-03 16:53:24 +0300

    feature: added auth and login api , half work

---
+++ src/app/App.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './App.css'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import {useSelector} from 'react-redux'
+import {AppRootStateType} from './store'
+import {RequestStatusType} from './app-reducer'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import LinearProgress from '@mui/material/LinearProgress';
+import {Menu} from '@mui/icons-material';
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Navigate, Route, Routes} from 'react-router-dom'
+import {Login} from "../features/login";
+
+type PropsType = {
+    demo?: boolean
+}
+
+function App({demo = false, req, res}: any) {
+    const status = useSelector<AppRootStateType, RequestStatusType>((state) => state.app.status)
+    const isAuth = useSelector<AppRootStateType, boolean>((state) => state.app.isAuth)
+
+    return (
+        <div className="App">
+            <ErrorSnackbar/>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+                {status === 'loading' && <LinearProgress/>}
+            </AppBar>
+            <Container fixed>
+                <Routes>
+                    <Route path={'/'} element={<TodolistsList demo={demo}/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'*'} element={<h1>NOY FOUND 404</h1>}/>
+                </Routes>
+            </Container>
+        </div>
+    )
+}
+
+export default App
